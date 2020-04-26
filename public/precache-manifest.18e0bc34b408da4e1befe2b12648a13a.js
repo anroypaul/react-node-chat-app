dd314270d1777ff5f7ed386fd71d544fd1691577
paulkoval <anroypaul@gmail.com>
2020-04-26 13:55:09 -0700

    build

---
+++ public/precache-manifest.18e0bc34b408da4e1befe2b12648a13a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d67c42a82345622a683aa773a346303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba74de2b0776c9b48e96",
+    "url": "/static/js/2.a15c2378.chunk.js"
+  },
+  {
+    "revision": "eef4f39967929a7a4fef470509b56e0c",
+    "url": "/static/js/2.a15c2378.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388a3ab3b4459fcf29b9",
+    "url": "/static/js/main.607bd94e.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
